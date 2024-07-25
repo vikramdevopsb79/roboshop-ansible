71d92384b9a7f5151147b1ef0935493afbff9764
sai <kuppalasaivikram@gmail.com>
2024-07-25 17:35:52 +0530

    roboshop ansible frontend

---
+++ run.sh
@@ -0,0 +1,5 @@
+if [ -z "$component" -o -z "$env" -o -z "$ssh_password" ]; then
+  echo Input missing from command line
+  exit 1
+fi
+ansible-playbook -i $component-$env.vikramdevops.store, -e env=$env -e ansible_user=ec2-user -e ansible_password=$ssh_password -e component=$component main.yml
