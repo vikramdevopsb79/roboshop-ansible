1d25d3810b97a22724b5a644b2d602d2ac36e012
sai <kuppalasaivikram@gmail.com>
2024-07-27 16:57:53 +0530

    provider added for vault

--- run.sh
+++ run.sh
@@ -3,3 +3,5 @@ if [ -z "$component" -o -z "$env" -o -z "$ssh_password" ]; then
   exit 1
 fi
 ansible-playbook -i $component-$env.vikramdevops.store, -e env=$env -e ansible_user=ec2-user -e ansible_password=$ssh_password -e component=$component main.yml
+
+# ssh_password=DevOps321 env=dev component=cart bash run.sh
